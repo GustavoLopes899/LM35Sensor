0332c0080fa29bab619c8b708dc6d9c206771f40
Gustavo <GustavoLopes899@gmail.com>
2018-08-29 11:00:55 -0300

    Led's operations removed

--- LM35_Sensor.ino
+++ LM35_Sensor.ino
@@ -1,19 +1,13 @@
 // Program to use a LM35 temperature's sensor with Arduino //
 
-const int led = 8;
 const int LM35 = A3;
 float temperature;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(led, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(led, HIGH);
-  delay(1000);
-  digitalWrite(led, LOW);
-  delay(1000);
   temperature = (float(analogRead(LM35)) * 5 / (1023)) / 0.01;
   Serial.print("Temperature: ");
   Serial.println(temperature);
