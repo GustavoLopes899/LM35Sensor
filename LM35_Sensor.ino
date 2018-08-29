ad4abcdc6f0a87bbfa1dc3fbaab26c72d7fa7e28
Gustavo <GustavoLopes899@gmail.com>
2018-08-29 11:06:30 -0300

    The reference voltage variable was created

--- LM35_Sensor.ino
+++ LM35_Sensor.ino
@@ -1,5 +1,6 @@
 // Program to use a LM35 temperature's sensor with Arduino //
 
+const float voltage_reference = 5;
 const int LM35 = A3;
 float temperature;
 
@@ -8,7 +9,7 @@ void setup() {
 }
 
 void loop() {
-  temperature = (float(analogRead(LM35)) * 5 / (1023)) / 0.01;
+  temperature = (float(analogRead(LM35)) * voltage_reference / (1023)) / 0.01;
   Serial.print("Temperature: ");
   Serial.println(temperature);
 }
