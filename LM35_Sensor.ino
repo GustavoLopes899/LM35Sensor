dfd9e94aafcb72bc1e4b6b9f45e13c0090827d02
Gustavo <GustavoLopes899@gmail.com>
2018-09-11 14:20:05 -0300

    Fix problems

--- LM35_Sensor.ino
+++ LM35_Sensor.ino
@@ -1,4 +1,5 @@
-// Program to use a LM35 temperature's sensor with Arduino //
+// Program to use a LM35 temperature's sensor with Arduino UNO //
+// Have some problems with Arduino MEGA //
 
 const float voltage_reference = 5;      // used to change the reference's voltage, could be changed depending of the board used
 const int LM35 = A3;                    // sensor pin
@@ -9,7 +10,7 @@ void setup() {
 }
 
 void loop() {
-  temperature = (float(analogRead(LM35)) * voltage_reference / (1023)) / 0.01;
+  temperature = (voltage_reference * analogRead(LM35) * 100.0) / 1024;
   Serial.print("Temperature: ");
   Serial.println(temperature);
 }
